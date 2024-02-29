a007bfc912375c65a7770afe47d073a033aba0f0
yetone <yetoneful@gmail.com>
2024-02-29 11:53:33 +0800

    feat: reproduce cannot resize window if has data-tauri-drag-region

--- src/App.tsx
+++ src/App.tsx
@@ -12,39 +12,48 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div>
+      <div style={{
+        height: '100px',
+        width: '100%',
+        backgroundColor: 'red',
+      }} data-tauri-drag-region />
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div className="container">
 
-      <p>
-        Click on the Tauri, Vite, and React logos to learn more about each
-        framework.
-      </p>
+        <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+        <div className="row">
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          </a>
+          <a href="https://tauri.app" target="_blank">
+            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
         </div>
+
+        <p>
+          Click on the Tauri, Vite, and React logos to learn more about each
+          framework.
+        </p>
+
+        <div className="row">
+          <div>
+            <input
+              id="greet-input"
+              onChange={(e) => setName(e.currentTarget.value)}
+              placeholder="Enter a name..."
+            />
+            <button type="button" onClick={() => greet()}>
+              Greet
+            </button>
+          </div>
+        </div>
+        <p>{greetMsg}</p>
       </div>
-      <p>{greetMsg}</p>
     </div>
   );
 }
